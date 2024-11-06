867ce1f17173617f409fdece2bdc0ed2d91c32ab
rawl0408 <raulin4bcn@gmail.com>
2024-11-06 14:02:31 +0100

    Sprint 6 Ejercicio 1 90%

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { BudgetsListComponent } from '../budgets-list/budgets-list.component';
+import { CurrentBudget } from '../models/budget';
+import { BudgetService } from '../services/budget.service';
+import { CommonModule } from '@angular/common';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'home-component',
   standalone: true,
-  imports: [WelcomeComponent, BudgetsListComponent],
+  imports: [WelcomeComponent, BudgetsListComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  budgets: CurrentBudget[] = [];
+  budgetForm: FormGroup;
+  selectedPrices: number = 0;
+
+  constructor(private budgetService: BudgetService, private fb: FormBuilder) {
+    this.budgetForm = this.fb.group({
+      budgetsArray: this.fb.array([])
+    });
+  }
+
+  ngOnInit(): void {
+    this.budgets = this.budgetService.getFrases();
+    const budgetControls = this.budgetForm.get('budgetsArray') as FormArray;
+
+    this.budgets.forEach(() => budgetControls.push(this.fb.control(false)));
+
+    budgetControls.valueChanges.subscribe((checkedStates: boolean[]) => {
+      this.selectedPrices = checkedStates.reduce((total, isChecked, index) => {
+        return isChecked ? total + this.budgets[index].price : total;
+      }, 0);
+    });
+  }
 
 }
--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CurrentBudget } from '../models/budget';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
 
-  constructor() { }
+  private budgetData: CurrentBudget[] = [
+    {
+      name: 'Seo',
+      description: "Programació d'una web responsive completa",
+      price: 300,
+    },
+    {
+      name: 'Ads',
+      description: "Programació d'una web responsive completa",
+      price: 400,
+    },
+    {
+      name: 'Web',
+      description: "Programació d'una web responsive completa",
+      price: 500,
+    },
+  ];
+
+  getFrases(): CurrentBudget[] {
+    return this.budgetData;
+  }
 }
